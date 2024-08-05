219da11662715d36a03ba766a0392acd1285f0ab
Mohamed-Parvez <clash.parwesh@gmail.com>
2024-08-05 20:11:50 +0530

    add logout api

--- api/routes/auth.router.js
+++ api/routes/auth.router.js
@@ -2,6 +2,7 @@ import express from "express";
 import signup from "../controllers/authControllers/signup.js";
 import login from "../controllers/authControllers/login.js";
 import refresh from "../controllers/authControllers/refresh.js";
+import logout from "../controllers/authControllers/logout.js";
 import cookieParser from "cookie-parser";
 
 const router = express.Router();
@@ -10,5 +11,6 @@ router.use(cookieParser());
 router.post("/signup", signup);
 router.post("/login", login);
 router.get("/refresh", refresh);
+router.post("/logout", logout);
 
 export default router;
